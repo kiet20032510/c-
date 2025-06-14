6a0f26a8cac07a2c25336e19b8cab1f8046b5de3
kiet20032510 <137425629+kiet20032510@users.noreply.github.com>
2025-06-14 19:36:46 +0700

    Add files via upload

---
+++ bt1/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ss1.bt1
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        public double Balance {  get; private set; }
+        public BankAccount(string accountNumber)
+        {
+            this.accountNumber = accountNumber;
+            this.Balance = 0;
+        }
+        public void Deposit(double amout)
+        {
+            if (amout > 0)
+            {
+                Balance += amout;
+            }
+            else
+            {
+                Console.WriteLine( "vui long thu lai");
+            }
+        }
+        public void Withdraw(double amount) {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance = Balance - amount;
+            }
+            else { Console.WriteLine("vui long thu lai");
+            }
+        }
+        
+
+    }
+
+}
---
+++ bt1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ss1.bt1
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            var acc = new BankAccount("123456");
+            acc.Deposit(1000);
+            acc.Withdraw(200);
+            Console.WriteLine(acc.Balance);
+        }
+    }
+}
