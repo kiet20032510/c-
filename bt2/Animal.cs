55eebf03b980444e983b1d34e52e90f89a1968a0
kiet20032510 <137425629+kiet20032510@users.noreply.github.com>
2025-06-14 21:35:42 +0700

    Add files via upload

---
+++ bt2/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bt2
+{
+    public class Animal
+    {
+        protected string name = "CAT";
+    }
+    public class Dog : Animal 
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Ten cua con vat la: " + name);
+        }
+    }
+}
